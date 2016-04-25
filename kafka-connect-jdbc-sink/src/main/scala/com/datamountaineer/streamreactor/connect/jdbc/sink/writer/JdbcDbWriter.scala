fbf12d87e0a5066ad66ec852b7d84865480c5141
stheppi <stefan.bocutiu@gmail.com>
2016-04-26 00:37:28 +0100

    adding unit tests + adding connection pool

--- kafka-connect-jdbc-sink/src/main/scala/com/datamountaineer/streamreactor/connect/jdbc/sink/writer/JdbcDbWriter.scala
+++ kafka-connect-jdbc-sink/src/main/scala/com/datamountaineer/streamreactor/connect/jdbc/sink/writer/JdbcDbWriter.scala
@@ -1,11 +1,12 @@
 package com.datamountaineer.streamreactor.connect.jdbc.sink.writer
 
-import java.sql.{DriverManager, SQLException}
+import java.sql.SQLException
 
 import com.datamountaineer.streamreactor.connect.jdbc.sink.StructFieldsDataExtractor
 import com.datamountaineer.streamreactor.connect.jdbc.sink.config.{ErrorPolicyEnum, JdbcSinkSettings}
 import com.datamountaineer.streamreactor.connect.sink._
 import com.typesafe.scalalogging.slf4j.StrictLogging
+import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
 import org.apache.kafka.connect.sink.SinkRecord
 
 import scala.util.Try
@@ -22,7 +23,12 @@ case class JdbcDbWriter(connectionStr: String,
                         statementBuilder: PreparedStatementBuilder,
                         errorHandlingPolicy: ErrorHandlingPolicy) extends DbWriter with StrictLogging {
 
-  val connection = DriverManager.getConnection(connectionStr)
+  private val config = new HikariConfig()
+  config.setJdbcUrl(connectionStr)
+  //config.setUsername("bart");
+  //config.setPassword("51mp50n");
+  private val ds = new HikariDataSource(config)
+  //val connection = DriverManager.getConnection(connectionStr)
 
   /**
     * Writes the given records to the database
@@ -33,43 +39,52 @@ case class JdbcDbWriter(connectionStr: String,
     if (records.isEmpty)
       logger.warn("Received empty sequence of SinkRecord")
     else {
-      val statemens = statementBuilder.build(records)(connection)
-      if (statemens.nonEmpty) {
-        try {
-          //begin transaction
-          connection.setAutoCommit(false)
-          statemens.foreach { statement =>
-            statement.execute()
-          }
-          //commit the transaction
-          connection.commit()
-        }
-        catch {
-          case sqlException: SQLException =>
-            logger.error(
-              s"""
-                 | Following error has occurred inserting data starting at
-                 | topic:${records.head.topic()}
-                 | offset:${records.head.kafkaOffset()}
-                 | partition:${records.head.kafkaPartition()}""".stripMargin)
 
-            //rollback the transaction
-            connection.rollback()
-            errorHandlingPolicy.handle(records, sqlException)(connection)
-        }
-        finally {
-          Try {
-            statemens.foreach(_.close())
+      val connection = ds.getConnection
+      try {
+        val statemens = statementBuilder.build(records)(connection)
+        if (statemens.nonEmpty) {
+          try {
+            //begin transaction
+            connection.setAutoCommit(false)
+            statemens.foreach { statement =>
+              statement.execute()
+            }
+            //commit the transaction
+            connection.commit()
           }
+          catch {
+            case sqlException: SQLException =>
+              logger.error(
+                s"""
+                   | Following error has occurred inserting data starting at
+                   | topic:${records.head.topic()}
+                   | offset:${records.head.kafkaOffset()}
+                   | partition:${records.head.kafkaPartition()}""".stripMargin)
+
+              //rollback the transaction
+              connection.rollback()
+              errorHandlingPolicy.handle(records, sqlException)(connection)
+          }
+          finally {
+            Try {
+              statemens.foreach(_.close())
+            }
+          }
+        }
+      }
+      finally {
+        if (connection == null) {
+          connection.close()
         }
       }
     }
   }
 
-  override def close(): Unit = {
-    connection.close()
-  }
 
+  override def close(): Unit = {
+    ds.close()
+  }
 }
 
 object JdbcDbWriter {
--- kafka-connect-jdbc-sink/src/test/scala/com/datamountaineer/streamreactor/connect/jdbc/sink/config/writer/SinglePreparedStatementBuilderTest.scala
+++ kafka-connect-jdbc-sink/src/test/scala/com/datamountaineer/streamreactor/connect/jdbc/sink/config/writer/SinglePreparedStatementBuilderTest.scala
@@ -2,8 +2,8 @@ package com.datamountaineer.streamreactor.connect.jdbc.sink.config.writer
 
 import java.sql.{Connection, PreparedStatement}
 
+import com.datamountaineer.streamreactor.connect.jdbc.sink._
 import com.datamountaineer.streamreactor.connect.jdbc.sink.writer.SinglePreparedStatementBuilder
-import com.datamountaineer.streamreactor.connect.jdbc.sink.{BooleanPreparedStatementBinder, IntPreparedStatementBinder, PreparedStatementBinder, StructFieldsDataExtractor}
 import org.apache.kafka.connect.data.{Schema, SchemaBuilder, Struct}
 import org.apache.kafka.connect.sink.SinkRecord
 import org.mockito.Matchers._
@@ -17,35 +17,49 @@ class SinglePreparedStatementBuilderTest extends WordSpec with Matchers with Moc
       val valueExtractor = mock[StructFieldsDataExtractor]
       val dataBinders = Seq[(String, PreparedStatementBinder)](
         "colA" -> BooleanPreparedStatementBinder(true),
-        "colB" -> IntPreparedStatementBinder(3))
+        "colB" -> IntPreparedStatementBinder(3),
+        "colC" -> LongPreparedStatementBinder(124566),
+        "colD" -> StringPreparedStatementBinder("somevalue"),
+        "colE" -> DoublePreparedStatementBinder(-5345.22),
+        "colF" -> FloatPreparedStatementBinder(0),
+        "colG" -> BytePreparedStatementBinder(-24),
+        "colH" -> ShortPreparedStatementBinder(-2345))
 
 
       when(valueExtractor.get(any[Struct])).thenReturn(dataBinders)
       val builder = SinglePreparedStatementBuilder("tableA", valueExtractor)
 
+      //schema is not used as we mocked the value extractors
       val schema = SchemaBuilder.struct.name("record")
         .version(1)
         .field("id", Schema.STRING_SCHEMA)
-        .field("int_field", Schema.INT32_SCHEMA)
-        .field("long_field", Schema.INT64_SCHEMA)
-        .field("string_field", Schema.STRING_SCHEMA)
         .build
 
       val record = new Struct(schema)
       val records = (1 to 10).map(v => new SinkRecord("aa", 1, null, null, schema, record, v))
 
       val connection = mock[Connection]
-      val sql="INSERT INTO tableA(colA,colB) VALUES(?,?)"
+      val sql = "INSERT INTO tableA(colA,colB,colC,colD,colE,colF,colG,colH) VALUES(?,?,?,?,?,?,?,?)"
 
-      /*val statements = (1 to 10).map(v=>mock[PreparedStatement]).toArray
-      when(connection.prepareStatement(sql))
-        .thenReturn(statements:_*)
+      val statements = (1 to 10).map(v => mock[PreparedStatement])
+      when(connection.prepareStatement(sql)).thenReturn(statements.head, statements.tail: _*)
 
-      val statements = builder.build(records)(connection)
+      val actualStatements = builder.build(records)(connection)
 
-      statements.size shouldBe 10
+      actualStatements.size shouldBe 10
       verify(connection, times(10)).prepareStatement(sql)
-      */
+
+      statements.foreach { s =>
+        verify(s, times(1)).setBoolean(1, true)
+        verify(s, times(1)).setInt(2, 3)
+        verify(s, times(1)).setLong(3, 124566)
+        verify(s, times(1)).setString(4, "somevalue")
+        verify(s, times(1)).setDouble(5, -5345.22)
+        verify(s, times(1)).setFloat(6, 0)
+        verify(s, times(1)).setByte(7, -24)
+        verify(s, times(1)).setShort(8, -2345)
+      }
+
     }
   }
 }
