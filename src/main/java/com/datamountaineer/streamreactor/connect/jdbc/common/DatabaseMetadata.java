9957c9826cf0f76cb810cae1c45a60dc34b34d21
Andrew Stevenson <andrew@datamountaineer.com>
2016-05-26 10:58:16 +0200

    fixing my stupidity

--- src/main/java/com/datamountaineer/streamreactor/connect/jdbc/common/DatabaseMetadata.java
+++ src/main/java/com/datamountaineer/streamreactor/connect/jdbc/common/DatabaseMetadata.java
@@ -25,7 +25,6 @@ import org.slf4j.LoggerFactory;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
