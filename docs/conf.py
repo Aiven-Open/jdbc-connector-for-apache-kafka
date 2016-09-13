31ef60c58399ec64b180d479ad44abeee5c7db70
Jason Gustafson <jason@confluent.io>
2016-09-13 13:09:28 -0700

    Fix short version in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ copyright = u'2015, Confluent, Inc.'
 # built documents.
 #
 # The short X.Y version.
-version = '3.0'
+version = '3.1'
 # The full version, including alpha/beta/rc tags.
 release = '3.1.0-SNAPSHOT'
 
