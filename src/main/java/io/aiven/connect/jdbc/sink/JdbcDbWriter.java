0a308a74bf6260483b20eef698f8df2790d8150c
JASH-PATEL-6566 <pateljash1511.com>
2024-11-29 19:43:57 -0400

    Solve-Smell: solve Insufficient Modularization for JdbcWriter class, extract 2 more class BufferManager and TableNameGenerator

--- src/main/java/io/aiven/connect/jdbc/sink/JdbcDbWriter.java
+++ src/main/java/io/aiven/connect/jdbc/sink/JdbcDbWriter.java
@@ -17,39 +17,27 @@
 
 package io.aiven.connect.jdbc.sink;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-
-import org.apache.kafka.connect.errors.ConnectException;
-import org.apache.kafka.connect.sink.SinkRecord;
-
 import io.aiven.connect.jdbc.dialect.DatabaseDialect;
 import io.aiven.connect.jdbc.util.CachedConnectionProvider;
 import io.aiven.connect.jdbc.util.TableId;
-
+import org.apache.kafka.connect.sink.SinkRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Collection;
 
 public class JdbcDbWriter {
 
     private static final Logger log = LoggerFactory.getLogger(JdbcDbWriter.class);
 
-    private static final Pattern NORMALIZE_TABLE_NAME_FOR_TOPIC = Pattern.compile("[^a-zA-Z0-9_]");
-
     private final JdbcSinkConfig config;
-
     private final DatabaseDialect dbDialect;
-
     private final DbStructure dbStructure;
-
     final CachedConnectionProvider cachedConnectionProvider;
 
-    JdbcDbWriter(final JdbcSinkConfig config, final DatabaseDialect dbDialect, final DbStructure dbStructure) {
+    public JdbcDbWriter(final JdbcSinkConfig config, final DatabaseDialect dbDialect, final DbStructure dbStructure) {
         this.config = config;
         this.dbDialect = dbDialect;
         this.dbStructure = dbStructure;
@@ -63,62 +51,28 @@ public class JdbcDbWriter {
         };
     }
 
-    void write(final Collection<SinkRecord> records) throws SQLException {
+    public void write(final Collection<SinkRecord> records) throws SQLException {
         final Connection connection = cachedConnectionProvider.getConnection();
+        final BufferManager bufferManager = new BufferManager(config, dbDialect, dbStructure, connection);
 
-        final Map<TableId, BufferedRecords> bufferByTable = new HashMap<>();
-        for (final SinkRecord record : records) {
-            final TableId tableId = destinationTable(record.topic());
-            BufferedRecords buffer = bufferByTable.get(tableId);
-            if (buffer == null) {
-                buffer = new BufferedRecords(config, tableId, dbDialect, dbStructure, connection);
-                bufferByTable.put(tableId, buffer);
-            }
-            buffer.add(record);
+        for (SinkRecord record : records) {
+            bufferManager.addRecord(record);
         }
-        for (final Map.Entry<TableId, BufferedRecords> entry : bufferByTable.entrySet()) {
-            final TableId tableId = entry.getKey();
-            final BufferedRecords buffer = entry.getValue();
-            log.debug("Flushing records in JDBC Writer for table ID: {}", tableId);
-            buffer.flush();
-            buffer.close();
-        }
+
+        bufferManager.flushAndClose();
         connection.commit();
     }
 
-    void closeQuietly() {
+    public void closeQuietly() {
         cachedConnectionProvider.close();
     }
 
     TableId destinationTable(final String topic) {
-        final String tableName = generateTableNameFor(topic);
+        final String tableName = TableNameGenerator.generateTableName(config, topic);
         return dbDialect.parseTableIdentifier(tableName);
     }
 
     public String generateTableNameFor(final String topic) {
-        String tableName = config.tableNameFormat.replace("${topic}", topic);
-        if (config.tableNameNormalize) {
-            tableName = NORMALIZE_TABLE_NAME_FOR_TOPIC.matcher(tableName).replaceAll("_");
-        }
-        if (!config.topicsToTablesMapping.isEmpty()) {
-            tableName = config.topicsToTablesMapping.getOrDefault(topic, "");
-        }
-        if (tableName.isEmpty()) {
-            final String errorMessage =
-                    String.format(
-                            "Destination table for the topic: '%s' "
-                                    + "couldn't be found in the topics to tables mapping: '%s' "
-                                    + "and couldn't be generated for the format string '%s'",
-                            topic,
-                            config.topicsToTablesMapping
-                                    .entrySet()
-                                    .stream()
-                                    .map(e -> String.join("->", e.getKey(), e.getValue()))
-                                    .collect(Collectors.joining(",")),
-                            config.tableNameFormat);
-            throw new ConnectException(errorMessage);
-        }
-        return tableName;
+        return TableNameGenerator.generateTableName(config, topic);
     }
-
 }
